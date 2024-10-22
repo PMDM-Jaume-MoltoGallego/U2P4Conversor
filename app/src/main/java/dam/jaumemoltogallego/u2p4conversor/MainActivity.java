ce89fdbb79100d06dbd973569973e582afb176e8
Jaume Moltó Gallego <jaume@MacBook-Air-de-Jaume-2.local>
2024-10-22 19:43:23 +0200

    Guided

--- app/src/main/java/dam/jaumemoltogallego/u2p4conversor/MainActivity.java
+++ app/src/main/java/dam/jaumemoltogallego/u2p4conversor/MainActivity.java
@@ -1,6 +1,10 @@
 package dam.jaumemoltogallego.u2p4conversor;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setUI();
+    }
+
+    private void setUI(){
+        EditText etPulgada = findViewById(R.id.et_pulgada);
+        EditText etResultado = findViewById(R.id.et_Resultado);
+        Button buttonConvertir = findViewById(R.id.button_Convertir);
+
+        buttonConvertir.setOnClickListener(view ->{
+            try {
+                etResultado.setText(convertir(etPulgada.getText().toString()));
+            }catch (Exception e){
+                Log.e("LogsConversor", e.getMessage());
+            }
+        });
+    }
+
+    private String convertir(String pulgadaText){
+        double pulgadaValue = Double.parseDouble(pulgadaText)*2.54;
+
+        return String.format("%.2f",pulgadaValue);
     }
 }
