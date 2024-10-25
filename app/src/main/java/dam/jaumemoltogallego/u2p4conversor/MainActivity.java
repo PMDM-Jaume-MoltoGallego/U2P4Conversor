57167a3a29c5eda68ee70f66e1928f5df8c9440e
Jaume Moltó Gallego <jaume@MacBook-Air-de-Jaume-2.local>
2024-10-25 18:08:12 +0200

    Actividad 3 realizada + errores anteriores

--- app/src/main/java/dam/jaumemoltogallego/u2p4conversor/MainActivity.java
+++ app/src/main/java/dam/jaumemoltogallego/u2p4conversor/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +19,7 @@ import com.google.android.material.chip.Chip;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
+    private final String DEBUG_TAG = this.getClass().getSimpleName();
     private Boolean b = true;
 
     @Override
@@ -35,6 +37,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //TODO Sirve para cuando se ejecuta por primera vez la aplicación,
+    // cuando ponemos en primer plano nuestra aplicación desde las aplicaciones abiertas
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(DEBUG_TAG,"Volvemos con la APP");
+    }
+
+    //TODO Sirve para varias utilidades,como para cuando le damos al boton de atrás,
+    // cuando le damos al boton HOME teniendo la aplicación en primer plano,
+    // cuando accedemos a las aplicaciones abiertas
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(DEBUG_TAG,"Pausamos la APP");
+    }
+
+    //TODO Sirve para cuando el usuario le da al boton de atrás o lo hace el sistema
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(DEBUG_TAG,"Cierra la APP");
+    }
+
+    //TODO Sirve para cuando se ejecuta por primera vez la aplicación,
+    // cuando ponemos en primer plano nuestra aplicación desde las aplicaciones abiertas
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(DEBUG_TAG,"Empezamos la APP");
+    }
+
+    //TODO Sirve para cuando le damos al boton de atrás,
+    // cuando le damos al boton HOME teniendo la aplicación en primer plano,
+    // cuando accedemos a las aplicaciones abiertas
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(DEBUG_TAG,"Paramos la APP");
+    }
+
+    //TODO Sirve para cuando ponemos en primer plano nuestra aplicación desde las aplicaciones abiertas
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(DEBUG_TAG,"Volvemos a utilizar la APP");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
     private void setUI() {
         EditText etPulgada = findViewById(R.id.et_pulgada);
         TextView etResultado = findViewById(R.id.et_resultado);
@@ -48,12 +108,14 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO Cambiamos el valor del boolean para saber si estamos de una forma u otra y actualizamos los textos
         incm.setOnClickListener(v -> {
+            Log.i(DEBUG_TAG, "Botón in-cm pulsado");
             b = !b;
-            actualizarTexto(textView,incm);
+            actualizarTexto(textView,incm, etPulgada);
         });
 
         //TODO convertimos los datos a pulgadas o centímetros
         buttonConvertir.setOnClickListener(v -> {
+            Log.i(DEBUG_TAG, "Botón Convertir pulsado");
             try {
                 String dato = etPulgada.getText().toString();
                 etResultado.setText(convertirDoble(dato));
@@ -69,13 +131,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //TODO Actualizamos el texto del título azul y el texto del chip
-    private void actualizarTexto(TextView textView, Chip incm) {
+    private void actualizarTexto(TextView textView, Chip incm , EditText etPulgada)  {
         if (b) {
             textView.setText("Conversión de Centímetros a Pulgadas");
+            etPulgada.setText("Introduce los centímetros");
             incm.setText("cm-in");
             incm.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
         } else {
             textView.setText("Conversión de Pulgadas a Centímetros");
+            etPulgada.setText("Introduce las pulgadas");
             incm.setText("in-cm");
             incm.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
         }
